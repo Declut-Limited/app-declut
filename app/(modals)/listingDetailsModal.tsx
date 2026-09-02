3276d1bb6ff61bc032deef4b75e9e3e07c9a02b6
idowudamolaolatunji <idowudamolaolatunji@gmail.com>
2026-09-02 17:01:49 +0100

    fixes

--- app/(modals)/listingDetailsModal.tsx
+++ app/(modals)/listingDetailsModal.tsx
@@ -717,7 +717,7 @@ const styles = StyleSheet.create({
     shadowColor: colors.black,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.08,
-    shadowRadius: 6,a
+    shadowRadius: 6,
     elevation: 3,
   },
   floatingHeaderRow: {
